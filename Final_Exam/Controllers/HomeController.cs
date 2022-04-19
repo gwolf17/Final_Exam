0559d48b2d0e533261d586e2f71a61a7b8ea5acf
gwolf17 <90425429+gwolf17@users.noreply.github.com>
2022-04-19 11:22:15 -0600

    Add project files.

---
+++ Final_Exam/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Final_Exam.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Final_Exam.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
